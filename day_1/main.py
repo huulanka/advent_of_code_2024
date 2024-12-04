bcb7716931413a2946fab3836d02b07477e64414
hulanka <fensterstockhirs@icloud.com>
2024-12-04 21:23:30 +0100

    Day one / task two

--- day_1/main.py
+++ day_1/main.py
@@ -32,12 +32,50 @@ def calculate_difference(list_one, list_two):
         sum_of_difference += difference_abs
     return sum_of_difference
 
+# Count the number of times each value appears in the list
+def count_findings(list):
+    findings = []
+    while len(list) > 0:
+        counting = list.count(list[0])
+        value = list[0]
+        findings.append((value, counting))
+        list = [x for x in list if x != value]
+    return findings
+
+# Calculate findings score
+def calculate_score(findings_list_one, findings_list_two):
+    score = 0
+    for i in range(len(findings_list_one)):
+        value_list_one = findings_list_one[i][0]
+        counting_list_one = findings_list_one[i][1]
+        # Is value of list one in value of list two?
+        for j in range(len(findings_list_two)):
+            value_list_two = findings_list_two[j][0]
+            if value_list_one != value_list_two:
+                continue
+            counting_list_two = findings_list_two[j][1]
+            score += value_list_one * counting_list_one * counting_list_two
+            break
+
+    return score
+
+
 def main():
     raw_data = read_input()
     list_one, list_two = split_data(raw_data)
     list_one, list_two = order_list(list_one, list_two)
+
+    # Task 1
     sum_of_difference = calculate_difference(list_one, list_two)
     print(f'The sum of the difference between the two lists is: {sum_of_difference}')
+
+    # Task 2
+    findings_list_one = count_findings(list_one)
+    findings_list_two = count_findings(list_two)
+
+    score = calculate_score(findings_list_one, findings_list_two)
+    print(f'The score is: {score}')
+
         
 if __name__ == '__main__':
     main()
